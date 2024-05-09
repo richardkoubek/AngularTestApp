91651b8107a4efdcb05029ea59fd4046f4ad32ae
Richard Koubek <koubek.richard@gmail.com>
2024-05-09 17:50:59 +0200

    interpolation examples added

--- testapp/src/app/test/test.component.ts
+++ testapp/src/app/test/test.component.ts
@@ -2,9 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  templateUrl: './test.component.html',
+  template: `
+          <h2>
+          {{name}}
+          </h2>
+          <p>
+          {{2 + 2}}
+          </p>
+          `,
   styleUrl: './test.component.css'
 })
 export class TestComponent {
-
+  public name = "this also works";
+  constructor () { }
 }
